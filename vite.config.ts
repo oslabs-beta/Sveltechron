6bdf958d373d095099625427a21030e0a22daf4b
jayachankura <jayachankura@gmail.com>
2023-08-24 11:12:36 -0400

    cloned chrome-extension-svelte-typescript-boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { crx } from "@crxjs/vite-plugin";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import manifest from "./manifest.json";
 
+const srcDir = resolve(__dirname, "src");
+
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+    plugins: [svelte(), crx({ manifest })],
+    resolve: {
+        alias: {
+            src: srcDir,
+        },
+    },
 });
