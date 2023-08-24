139802ea8f1bab526725a63f3534d05daf2fb03e
jayachankura <jayachankura@gmail.com>
2023-08-23 23:51:26 -0400

    setup default formatter on svelte typescript app to prettier eslint, working with typescript and formats on save

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'sveltekit-adapter-chrome-extension';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
