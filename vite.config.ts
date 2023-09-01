526d89c878421e63b79e41772c7d6030c2e4bf75
jayachankura <jayachankura@gmail.com>
2023-08-31 21:32:19 -0400

    altered vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       dist: "public/devtools.html"
-  //       dist: "index.html"
-  //     }
-      
-  //   }
-  // }
-
 })
