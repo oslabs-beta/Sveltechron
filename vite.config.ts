33ec6591fc226277d200fc6ca6e9355bee104efc
Kevin Wong <kevwong.main@gmail.com>
2023-09-02 16:09:38 -0400

    Revert "altered vite config"
    This reverts commit 3df687890c4e10a3d0e98e8e85f9aef53f6e9a4e.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       dist: "public/devtools.html"
+  //       dist: "index.html"
+  //     }
+      
+  //   }
+  // }
+
 })
