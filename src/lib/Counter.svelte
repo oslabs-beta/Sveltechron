518ba3e5f23bcd14be1a92072bbf9c4ae962ba11
jayachankura <jayachankura@gmail.com>
2023-09-02 16:54:36 -0400

    configured svelte store and created a new store.ts file

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
-  let count: number = 0
+  import {count} from "../store" //need to destructure from named exports
+
   const increment = () => {
-    count += 1
+    count.update((n)=> n += 1)
+  }
+
+  let count_value: number;
+
+  count.subscribe((value) => count_value = value)
+
+  const reset = () => {
+    count.set(0)
   }
 </script>
 
+<!-- access example state from store.ts -->
 <button on:click={increment}>
-  count is {count}
+  count is {count_value}
+</button>
+
+<button on:click={reset} >
+click to reset the count to 0
 </button>
---
+++ src/store.ts
@@ -0,0 +1,4 @@
+import { writable } from 'svelte/store';
+
+//example store setup for the demo svelte boilerplate app
+export const count = writable(0) 
