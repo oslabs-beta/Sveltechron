ca74b077b8a003158db860423ea6ebe922e6e4b5
Kevin Wong <kevwong.main@gmail.com>
2023-09-02 16:09:38 -0400

    Revert "setup default formatter on svelte typescript app to prettier eslint, working with typescript and formats on save"
    This reverts commit 139802ea8f1bab526725a63f3534d05daf2fb03e.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'sveltekit-adapter-chrome-extension';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
