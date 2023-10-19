b7d0d263f2e3a6837a94db363ddbe52f0bfd4104
jayachankura <jayachankura@gmail.com>
2023-10-18 21:12:03 -0400

    successfully inject content scripts using previous vite build

--- extension/devtools/index.js
+++
@@ -1,9 +0,0 @@
-/**
-This script is run whenever the devtools are open.
-In here, we can create our panel.
-*/
-chrome.devtools.panels.create(
-  'SVOLTE',
-  '/icons/128.png',
-  './devtools/panel/panel.html'
-);
--- vite.config.ts
+++ vite.config.ts
@@ -7,18 +7,7 @@ const __dirname = path.resolve();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.json', '.svelte'],
-  },
   build: {
-    outDir: path.join(__dirname, 'extension/build'),
     target: 'esnext',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'bundle.js',
-        chunkFileNames: 'bundle.js',
-        assetFileNames: 'bundle.[ext]',
-      },
-    },
   },
 });
