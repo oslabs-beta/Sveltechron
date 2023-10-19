13951580c0ccedf75e3678e3447042a0443a33d1
jayachankura <jayachankura@gmail.com>
2023-10-18 20:29:56 -0400

    removed footer from devtools panel

---
+++ __tests__/example.test.ts
@@ -0,0 +1,31 @@
+//these must be imported each time
+import { fireEvent, render, screen } from '@testing-library/svelte';
+import '@testing-library/jest-dom';
+import Options from '../src/components/App.svelte';
+
+describe('Options.svelte', () => {
+  /**
+   * @author Jay Kura
+   * afterEach(() => cleanup()) is done automatically to clear the dom between tests
+   */
+  test('that shows count when rendered', () => {
+    render(Options, { count: 2 });
+    const count = screen.getByText('Current count:');
+    expect(count).toHaveTextContent('Current count: 2');
+  });
+  test('number increments when plus button is clicked', async () => {
+    render(Options, { count: 2 });
+    const plusButton = screen.getByText('+');
+    const count = screen.getByText('Current count:');
+    await fireEvent.click(plusButton);
+    expect(count).toHaveTextContent('Current count: 3');
+  });
+  test('number increments when minus button is clicked', async () => {
+    render(Options, { count: 10 });
+    console.log(screen.getByText('*'));
+    const minusButton = screen.getByText('-');
+    const count = screen.getByText('Current count:');
+    await fireEvent.click(minusButton);
+    expect(count).toHaveTextContent('Current count: 9');
+  });
+});
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
-import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  preprocess: [vitePreprocess(), preprocess({})],
+  preprocess: [vitePreprocess()],
 };
